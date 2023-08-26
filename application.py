62d4d3fd9d5e8d0cf4443fc2e299e224fdb95715
Dom Lamastra <dlamastra44@gmail.com>
2023-08-26 17:58:57 -0400

    added methods to project

--- application.py
+++ application.py
@@ -105,43 +105,55 @@ class Gym:
     def set_items(self, items):
         self.items = items
 
-# init driver object
-driver = webdriver.Chrome()
+def scroll_down(press_count):
 
-# navigate to the data page
-driver.get("https://www.serebii.net/")
+    # press down on the down arrow 50 times
+    time.sleep(random.randint(2, 5))
+    for i in range(press_count):
+        actions.send_keys(Keys.ARROW_DOWN).perform()
 
-# init an action chains object
-actions = ActionChains(driver)
+def make_click(xpath):
 
-# press down on the down arrow 50 times
-time.sleep(random.randint(2, 5))
-for i in range(50):
-    actions.send_keys(Keys.ARROW_DOWN).perform()
+    # click an element
+    time.sleep(random.randint(2, 5))
+    element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, xpath))).click()
 
-# click an element
-time.sleep(random.randint(2, 5))
-element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="lbar_ul"]/li[3]/ul/li[104]/a'))).click()
+def collect_all_data(xpath):
 
-# click an element
-time.sleep(random.randint(2, 5))
-element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="rbar"]/div[6]/ul/li[8]/a'))).click()
+    # getting data from xpath gyms
+    time.sleep(random.randint(2, 5))
+    element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, xpath)))
 
-# getting data from xpath gyms
-# for i in range(1, 9):
-time.sleep(random.randint(2, 5))
-element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="content"]/main/table/tbody/tr[2]/td[1]/table/tbody')))
+    gym_data = np.array(element.text.split('\n'), dtype=object)
+    gym = Gym()
+    for i in range(gym_data.size):
+        gym_data[i] = np.char.split(gym_data[i])
+    print(gym_data)
+    for i in range(gym_data.size):
+        for j in range(gym_data[i].size):
+            if (j == 0 and gym_data[i].size > 3):
+                if (gym_data[i][j] == 'Gym' and gym_data[i][j + 1] == 'Leader'):
+                    gym.set_leader_name(gym_data[i][j + 2])
 
-gym_data = np.array(element.text.split('\n'), dtype=object)
-gym = Gym()
-for i in range(gym_data.size):
-    gym_data[i] = np.char.split(gym_data[i])
-print(gym_data)
-for i in range(gym_data.size):
-    for j in range(gym_data[i].size):
-        if (j == 0 and gym_data[i].size > 3):
-            if (gym_data[i][j] == 'Gym' and gym_data[i][j + 1] == 'Leader'):
-                Gym.set_leader_name(gym_data[i][j + 2])
 
-element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="rbar"]/div[6]/ul/li[9]/a'))).click()
-print(driver.title)
+if __name__ == "__main__":
+
+    # init driver object
+    driver = webdriver.Chrome()
+
+    # navigate to the data page
+    driver.get("https://www.serebii.net/")
+
+    # init an action chains object
+    actions = ActionChains(driver)
+
+    scroll_down(50)
+
+    make_click('//*[@id="lbar_ul"]/li[3]/ul/li[104]/a')
+
+    make_click('//*[@id="rbar"]/div[6]/ul/li[8]/a')
+
+    collect_all_data('//*[@id="content"]/main/table/tbody/tr[2]/td[1]/table/tbody')
+
+    element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="rbar"]/div[6]/ul/li[9]/a'))).click()
+    print(driver.title)
