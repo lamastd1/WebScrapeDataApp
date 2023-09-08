8267c7876ef7f25e83f4f84a50e3e0e640f20fe7
Dom Lamastra <dlamastra44@gmail.com>
2023-09-07 21:56:30 -0400

    finalized kanto json

--- application.py
+++ application.py
@@ -7,9 +7,14 @@ from selenium.webdriver.common.by import By
 import random
 import chromedriver_binary
 import time
-import re
 import numpy as np
-from collections import defaultdict
+import json
+
+def scroll_up(press_count):
+     # press down on the down arrow 50 times
+    time.sleep(random.randint(2, 5))
+    for i in range(press_count):
+        actions.send_keys(Keys.ARROW_UP).perform()
 
 def scroll_down(press_count):
 
@@ -66,10 +71,13 @@ def collect_metadata(xpath):
         elif (hold[i].startswith("Reward")):
             fields_sep_by_comma = hold[i].split(",")
             badge = fields_sep_by_comma[0].split("Reward: ")
-            HM = fields_sep_by_comma[2].split(" ")
+            print(fields_sep_by_comma)
+            if (len(fields_sep_by_comma) > 2):
+                HM = fields_sep_by_comma[2].split(" ")
             return_dict['Badge'] = badge[1]
             return_dict['TM'] = fields_sep_by_comma[1]
-            return_dict['HM'] = HM[3]
+            if (len(fields_sep_by_comma) > 2):
+                return_dict['HM'] = HM[3]
 
     return return_dict
 
@@ -77,7 +85,11 @@ def collect_number_of_pokemon(xpath):
     time.sleep(random.randint(2, 5))
     element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, xpath)))
     fields = element.text.split(" ")
-    return len(fields) - 3
+    number_of_pokemon = len(fields) - 3
+    for i in range(len(fields)):
+        if ("." in fields[i]):
+            number_of_pokemon = number_of_pokemon - 1
+    return number_of_pokemon
 
 def collect_pokemon_name(xpath):
     time.sleep(random.randint(2, 5))
@@ -125,55 +137,69 @@ if __name__ == "__main__":
 
     # collect_all_data('//*[@id="content"]/main/table/tbody/tr[2]/td[1]/table/tbody')
 
-    text = collect_region('//*[@id="rbar"]/div[6]/ul/li[2]/a')
+    region = collect_region('//*[@id="rbar"]/div[6]/ul/li[2]/a')
+    region_dict[region] = []
 
-    # region_dict['Region']
+    for i in range(2, 17, 2):
 
-    gym_dict = {}
-    # xpath = '//*[@id="content"]/main/table/tbody/tr[1]/td/font'
-    gym_dict['Gym_Number'] = collect_gym_name('//*[@id="content"]/main/table/tbody/tr[1]/td/font')
+        gym_dict = {}
 
-    #print(region_dict)
+        gym_dict['Pokemon'] = []
+        # xpath = '//*[@id="content"]/main/table/tbody/tr[1]/td/font'
+        # //*[@id="content"]/main/table/tbody/tr[3]/td/font
+        gym_dict['Gym_Number'] = collect_gym_name('//*[@id="content"]/main/table/tbody/tr[' + str(i - 1) + ']/td/font')
 
-    gym_dict.update(collect_metadata('//*[@id="content"]/main/table/tbody/tr[2]/td[1]/p[1]'))
+        #print(region_dict)
 
-    number_of_pokemon = collect_number_of_pokemon('//*[@id="content"]/main/table/tbody/tr[2]/td[1]/table/tbody/tr[2]')
+        gym_dict.update(collect_metadata('//*[@id="content"]/main/table/tbody/tr[' + str(i) + ']/td[1]/p[1]'))
 
-    pokemon_dict = {}
+        number_of_pokemon = collect_number_of_pokemon('//*[@id="content"]/main/table/tbody/tr[' + str(i) + ']/td[1]/table/tbody/tr[2]')
 
-    # //*[@id="content"]/main/table/tbody/tr[2]/td[1]/table/tbody/tr[3]/td[2]
-    # //*[@id="content"]/main/table/tbody/tr[2]/td[1]/table/tbody/tr[3]/td[3]
-    # //*[@id="content"]/main/table/tbody/tr[2]/td[1]/table/tbody/tr[4]/td[1]
-    # //*[@id="content"]/main/table/tbody/tr[2]/td[1]/table/tbody/tr[4]/td[2]
-    # //*[@id="content"]/main/table/tbody/tr[2]/td[1]/table/tbody/tr[5]/td[1]
-    # //*[@id="content"]/main/table/tbody/tr[2]/td[1]/table/tbody/tr[5]/td[2]
+        # //*[@id="content"]/main/table/tbody/tr[2]/td[1]/table/tbody/tr[3]/td[2]
+        # //*[@id="content"]/main/table/tbody/tr[2]/td[1]/table/tbody/tr[3]/td[3]
+        # //*[@id="content"]/main/table/tbody/tr[2]/td[1]/table/tbody/tr[4]/td[1]
+        # //*[@id="content"]/main/table/tbody/tr[2]/td[1]/table/tbody/tr[4]/td[2]
+        # //*[@id="content"]/main/table/tbody/tr[2]/td[1]/table/tbody/tr[5]/td[1]
+        # //*[@id="content"]/main/table/tbody/tr[2]/td[1]/table/tbody/tr[5]/td[2]
 
-    # geodude 1st type
-    # //*[@id="content"]/main/table/tbody/tr[2]/td[1]/table/tbody/tr[4]/td[1]/a[1]
-    
-    gym_dict['Pokemon'] = []
-
-    for i in range(number_of_pokemon):
-        # //*[@id="content"]/main/table/tbody/tr[2]/td[1]/table/tbody/tr[2]/td[3]/a
-        pokemon_dict['Name'] = collect_pokemon_name('//*[@id="content"]/main/table/tbody/tr[2]/td[1]/table/tbody/tr[2]/td[' + str(i + 2) + ']/a')
-        pokemon_dict['Level'] = collect_pokemon_level('//*[@id="content"]/main/table/tbody/tr[2]/td[1]/table/tbody/tr[3]/td[' + str(i + 2) + ']')
-
+        # geodude 1st type
         # //*[@id="content"]/main/table/tbody/tr[2]/td[1]/table/tbody/tr[4]/td[1]/a[1]
-        # //*[@id="content"]/main/table/tbody/tr[2]/td[1]/table/tbody/tr[4]/td[1]/a[2]
-        # //*[@id="content"]/main/table/tbody/tr[4]/td[1]/table/tbody/tr[4]/td[1]/a
-        pokemon_dict['Types'] = []
-        
-        pokemon_dict['Types'].append(collect_pokemon_types('//*[@id="content"]/main/table/tbody/tr[2]/td[1]/table/tbody/tr[4]/td[' + str(i + 1) + ']/a'))
 
-        try: 
-            pokemon_dict['Types'].append(collect_pokemon_types('//*[@id="content"]/main/table/tbody/tr[2]/td[1]/table/tbody/tr[4]/td[' + str(i + 1) + ']/a[2]'))
-        except:
-            pass
+        for j in range(number_of_pokemon):
 
-        pokemon_dict['Moveset'] = collect_pokemon_moveset('//*[@id="content"]/main/table/tbody/tr[2]/td[1]/table/tbody/tr[5]/td[' + str(i + 1) + ']')
+            pokemon_dict = {}
+            # //*[@id="content"]/main/table/tbody/tr[2]/td[1]/table/tbody/tr[2]/td[3]/a
+            # //*[@id="content"]/main/table/tbody/tr[4]/td[1]/table/tbody/tr[2]/td[2]/a
+            # //*[@id="content"]/main/table/tbody/tr[6]/td[1]/table/tbody/tr[2]/td[2]/a
+            # //*[@id="content"]/main/table/tbody/tr[6]/td[1]/table/tbody/tr[2]/td[3]/a
+            # //*[@id="content"]/main/table/tbody/tr[6]/td[1]/table/tbody/tr[2]/td[4]/a
+            # print(number_of_pokemon)
+            # print(i)
+            # print(j + 2)
+            pokemon_dict['Name'] = collect_pokemon_name('//*[@id="content"]/main/table/tbody/tr[' + str(i) + ']/td[1]/table/tbody/tr[2]/td[' + str(j + 2) + ']/a')
 
-        gym_dict['Pokemon'].append(pokemon_dict)   
+            # //*[@id="content"]/main/table/tbody/tr[4]/td[1]/table/tbody/tr[3]/td[2]
+            pokemon_dict['Level'] = collect_pokemon_level('//*[@id="content"]/main/table/tbody/tr[' + str(i) + ']/td[1]/table/tbody/tr[3]/td[' + str(j + 2) + ']')
 
-    region_dict[gym_dict['Location']] = gym_dict
-    print(region_dict)
-    # element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="rbar"]/div[6]/ul/li[9]/a'))).click()
+            # //*[@id="content"]/main/table/tbody/tr[2]/td[1]/table/tbody/tr[4]/td[1]/a[1]
+            # //*[@id="content"]/main/table/tbody/tr[2]/td[1]/table/tbody/tr[4]/td[1]/a[2]
+            # //*[@id="content"]/main/table/tbody/tr[4]/td[1]/table/tbody/tr[4]/td[1]/a
+            pokemon_dict['Types'] = []
+            
+            pokemon_dict['Types'].append(collect_pokemon_types('//*[@id="content"]/main/table/tbody/tr[' + str(i) + ']/td[1]/table/tbody/tr[4]/td[' + str(j + 1) + ']/a'))
+
+            try: 
+                pokemon_dict['Types'].append(collect_pokemon_types('//*[@id="content"]/main/table/tbody/tr[' + str(i) + ']/td[1]/table/tbody/tr[4]/td[' + str(j + 1) + ']/a[2]'))
+            except:
+                pass
+
+            pokemon_dict['Moveset'] = collect_pokemon_moveset('//*[@id="content"]/main/table/tbody/tr[' + str(i) + ']/td[1]/table/tbody/tr[5]/td[' + str(j + 1) + ']')
+
+            gym_dict['Pokemon'].append(pokemon_dict)   
+
+        region_dict[region].append(gym_dict)
+        # print(region_dict)
+        # element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="rbar"]/div[6]/ul/li[9]/a'))).click()
+
+    json_data = json.dumps(region_dict, indent=2)
+    print(json_data)
